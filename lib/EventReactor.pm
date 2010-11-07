81608b0845744c42c02537ee7b31b54fb830db02
vti <viacheslav.t@gmail.com>
2010-11-07 21:53:49 +0100

    Added default SSL key and cert

--- lib/EventReactor.pm
+++ lib/EventReactor.pm
@@ -9,12 +9,51 @@ use EventReactor::Loop;
 use EventReactor::Timer;
 
 use Errno qw/EAGAIN EWOULDBLOCK EINPROGRESS/;
+use File::Spec;
 use IO::Socket;
 require Carp;
 
 use constant DEBUG => $ENV{EVENT_REACTOR_DEBUG} ? 1 : 0;
 
 use constant IO_SOCKET_SSL => eval { require IO::Socket::SSL; 1 };
+
+# openssl req -new -x509 -keyout cakey.pem -out cacert.pem -nodes -days 7300
+use constant SSL_KEY => <<'EOF';
+-----BEGIN PRIVATE KEY-----
+MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAMy4fNRWTlZ15y5w
+yZyWN5L3POBm0K6FtXJL8WPI201fjKc5Ye55/hF4YNFIi5tF7krX9KEW1BDzZTCw
+83lWAnkzpcgy4AuRPSiTWI1kINnOEtn3ae4+HYkIP9KIgVIPLHnuC5pzWBfdFBzU
+UYtz+3fwnsBNFVhqJT2Iej54wPdLAgMBAAECgYEAmUNk8PLMIx6RvMrjpT8wy+4c
+vUM75+xiMvd3+GRfCkYGXwsurgLWmu2sTgIpwk1QYOfcVN1qVmZh01ombShfIFaP
+Y1AUrwfaBW2bgiUyOXmK3WdgycpMrhGI0KOhAWu0K5D13FOgbS6yRVNJMbDujxuE
+m48qampjXZvO5jfwgoECQQDy+EWIIPvkF4fFX+VayIUceUdfz72HruVjcYjeaKEz
+nSjxN3HaV3iS0n5B+Ilb+rZ0PR5XNj6mN2Pp7wx0lotzAkEA17MYD+1wyPD29fRm
+alRJ8lFy8MV4UyMrOqnLX58HsmpWWHGUXb8gMSwvYqUeV3LVuDga0ucbU5KlJ5lV
+y3IeyQJAeRcO4DdAEn8/pTiTv9jrrjMrRS7tkG+z1wnAYsfWfzi2LPGrBlxNtS6+
+yfYpzvN2dxv2wRRByOkWHIKMvJZCzQJAI/J67hyaEULnRXInp0zIzhN43ltqhCB2
+Ud5+QD9WnwtNvIuhOEZj7Q36D6yI8/X1XDAteDx/t1vXHlRVkgRA0QJATGt4aU84
+NCzhfVIWR9wdMzuYkA8yaZrcUaKk3UKckxhGQOOwSFMB4HUbM9vTwz1VdFcjECWi
+lOvmNA4Kx9riLA==
+-----END PRIVATE KEY-----
+EOF
+
+use constant SSL_CERT => <<'EOF';
+-----BEGIN CERTIFICATE-----
+MIICZjCCAc+gAwIBAgIJAOfXxWTGTLcyMA0GCSqGSIb3DQEBBQUAMEwxCzAJBgNV
+BAYTAkRFMRMwEQYDVQQIDApTb21lLVN0YXRlMRMwEQYDVQQKDApSZUFuaW1hdG9y
+MRMwEQYDVQQDDApSZUFuaW1hdG9yMB4XDTEwMTEwNjIyMDYxMloXDTMwMTEwMTIy
+MDYxMlowTDELMAkGA1UEBhMCREUxEzARBgNVBAgMClNvbWUtU3RhdGUxEzARBgNV
+BAoMClJlQW5pbWF0b3IxEzARBgNVBAMMClJlQW5pbWF0b3IwgZ8wDQYJKoZIhvcN
+AQEBBQADgY0AMIGJAoGBAMy4fNRWTlZ15y5wyZyWN5L3POBm0K6FtXJL8WPI201f
+jKc5Ye55/hF4YNFIi5tF7krX9KEW1BDzZTCw83lWAnkzpcgy4AuRPSiTWI1kINnO
+Etn3ae4+HYkIP9KIgVIPLHnuC5pzWBfdFBzUUYtz+3fwnsBNFVhqJT2Iej54wPdL
+AgMBAAGjUDBOMB0GA1UdDgQWBBRPvH4ezMIfId3cqbEHnKMcPo2auzAfBgNVHSME
+GDAWgBRPvH4ezMIfId3cqbEHnKMcPo2auzAMBgNVHRMEBTADAQH/MA0GCSqGSIb3
+DQEBBQUAA4GBAEeySk5j1EAFkGphrKrODJG6UK/PmmDrUrFbr67cLZRwJ5qe+vGb
+tpDETZXAYGvbO+ECLeTYqQoLqQN86d66PIBdWoRoBnNgd9P49GhFGOvnEZ4d8VHI
+wKNBg8NajSWDIy9TVaaqrXggBTQNYNKsGiX3gDIwdooxE2dsEiYw+eE/
+-----END CERTIFICATE-----
+EOF
 
 our $VERSION = '0.0001';
 
@@ -45,7 +84,9 @@ sub new {
 
 sub build_slave { shift; EventReactor::Slave->new(@_) }
 
-sub secure { @_ > 1 ? $_[0]->{secure} = $_[1] : $_[0]->{secure} }
+sub secure    { @_ > 1 ? $_[0]->{secure}    = $_[1] : $_[0]->{secure} }
+sub key_file  { @_ > 1 ? $_[0]->{key_file}  = $_[1] : $_[0]->{key_file} }
+sub cert_file { @_ > 1 ? $_[0]->{cert_file} = $_[1] : $_[0]->{cert_file} }
 
 sub on_connect { @_ > 1 ? $_[0]->{on_connect} = $_[1] : $_[0]->{on_connect} }
 sub on_error   { @_ > 1 ? $_[0]->{on_error}   = $_[1] : $_[0]->{on_error} }
@@ -67,6 +108,11 @@ sub listen {
 
     if ($self->secure) {
         Carp::croak q/IO::Socket::SSL is required/ unless IO_SOCKET_SSL;
+
+        Carp::croak
+          q/Either both key_file AND cert_file must be specified or NEIGHER of them, so default can be used/
+          unless ($self->key_file && $self->cert_file)
+          || (!$self->key_file && !$self->cert_file);
     }
 
     my $address = $self->address;
@@ -238,12 +284,30 @@ sub _accept {
             return $atom->accepted;
         }
 
+        unless ($self->key_file && $self->cert_file) {
+            my $tmp_dir = File::Spec->tmpdir;
+
+            my $key_file  = File::Spec->catfile($tmp_dir, 'cakey.pem');
+            my $cert_file = File::Spec->catfile($tmp_dir, 'cacert.pem');
+
+            open my $key, '>', $key_file;
+            print $key SSL_KEY;
+            close $key;
+
+            open my $cert, '>', $cert_file;
+            print $cert SSL_CERT;
+            close $cert;
+
+            $self->key_file($key_file);
+            $self->cert_file($cert_file);
+        }
+
         $socket = IO::Socket::SSL->start_SSL(
             $socket,
             SSL_startHandshake => 0,
             SSL_server         => 1,
-            SSL_key_file       => 'cakey.pem',
-            SSL_cert_file      => 'cacert.pem',
+            SSL_key_file       => $self->key_file,
+            SSL_cert_file      => $self->cert_file,
         );
 
         unless ($socket) {
